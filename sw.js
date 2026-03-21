2f88014f0f475b971359dbdc520ef214569948c7
Bo Koehler <119698811+BoKoehler@users.noreply.github.com>
2026-03-21 14:24:38 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE = 'bloc-v1';
+const CACHE = 'bloc-v2';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
+  '/bloc/',
+  '/bloc/index.html',
+  '/bloc/manifest.json',
+  '/bloc/icon-192.png',
+  '/bloc/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
@@ -21,6 +23,8 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(cached =>
+      cached || fetch(e.request).catch(() => caches.match('/bloc/index.html'))
+    )
   );
 });
